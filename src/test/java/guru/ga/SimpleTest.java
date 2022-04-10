385a58206db963e26188dcab75f7b7da9e260526
Elena_Sukhanova <Elena4012678009>
2022-04-10 19:23:59 +0200

    new added build.gradle

---
+++ src/test/java/guru/ga/SimpleTest.java
@@ -0,0 +1,39 @@
+package guru.ga;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+    @BeforeAll
+    static void initDB(){
+        System.out.println("### @BeforeAll");
+    }
+    @BeforeEach
+
+    void openYaPage(){  Selenide.open("https://yandex.ru");
+        System.out.println("### @BeforeEach");
+    }
+    @AfterEach
+
+    void closeYaPage(){
+        System.out.println("### @AfterEach");
+        WebDriverRunner.closeWindow();
+          }
+    @AfterAll
+    static void cleanDB(){
+        System.out.println("### @AfterAll");
+    }
+
+    @Test
+    void assertTest(){
+        System.out.println("### @Test 0");
+ //...
+    }
+
+    @Test
+    void assertTest2(){
+        System.out.println("### @Test 1");
+    //...
+    }
+}
