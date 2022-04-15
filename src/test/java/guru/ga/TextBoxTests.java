120f8b192935982afa9d90a3b41e42076d330cb7
Elena_Sukhanova <Elena4012678009>
2022-04-15 11:13:38 +0200

    small refactor & added logger slf4j

---
+++ src/test/java/guru/ga/TextBoxTests.java
@@ -0,0 +1,15 @@
+package guru.ga;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @Test
+    void fillFormTest (){
+
+      open ("https://demoqa.com/automation-practice-form");
+    }
+
+
+}
